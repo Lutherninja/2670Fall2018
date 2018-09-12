545e863b683b36825c4061994f73d46aeff07171
Unknown <Luther.ninja@gmail.com>
2018-09-12 16:50:54 -0600

    Coded a Plnae
    Non grounded movement patterns and stuff

--- Fall2670Unity/Assets/Scripts/MovePattern.cs
+++ Fall2670Unity/Assets/Scripts/MovePattern.cs
@@ -11,25 +11,32 @@ public class MovePattern : ScriptableObject
 	
 	
 	
-	private Vector3 moveDirection;
+	protected Vector3 moveDirection;
 	private Vector3 rotDirection;
 
-	public void Invoke(CharacterController controller, Transform transform)
-	{	if (controller.isGrounded)
+	public virtual void Invoke(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded)
 		{
-			moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-			moveDirection = transform.TransformDirection(moveDirection);
-			rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-			transform.Rotate(rotDirection);
-				
-			if (Input.GetButton("Jump"))
-				moveDirection.y = jumpSpeed.Value;
-            
+			Move(transform);
 		}
+
+		Move(controller);
+	}
+
+	protected void Move(CharacterController controller)
+	{
 		moveDirection.y -= gravity.Value * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 	}
-	
+
+	protected void Move(Transform transform)
+	{
+		moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+		moveDirection = transform.TransformDirection(moveDirection);
+		rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+		transform.Rotate(rotDirection);
+	}
 }
 
 
